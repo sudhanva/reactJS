d21406feb96bf82d12f2ba8c0720e201c9d3b6f5
Sudhanva <sudhanva.sa@gmail.com>
2021-01-29 23:48:19 +0530

    added Header and Footer components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
